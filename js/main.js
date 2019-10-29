4a927f9bef7d1d904b4651ca6c083f89c5610c02
clintros <56990846+clintros@users.noreply.github.com>
2019-10-30 00:17:28 +1030

    changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-})
+
+// // $(window).scroll(function(i){
+//     $(document).on("scroll", function() {
+//     var scrollVar = $(window).scrollTop()
+//     if ($(scrollVar) > 200) {
+//         $('para').addClass("visible")
+//     }
+//     else {
+//         $('para').removeClass("visible")
+//     }
+// })
+
+// $(document).on("scroll", function () {
+//     var pageTop = $(document).scrollTop()
+//     var pageBottom = pageTop + $(window).height()
+//     var tags = $(".para")
+
+//     for (var i = 0; i < tags.length; i++)
+//     {
+//         var tag = tags[i]
+//         if ($(tag).position().top < pageBottom) {
+//             $(tag).addClass("visible")
+//         }
+//         else {
+//             $(tag).removeClass("visible")
+//         }
+//     }
+// })
